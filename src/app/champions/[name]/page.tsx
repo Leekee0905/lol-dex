11ca2e903f37452f1dfd389a7ce88d73aa05b4ac
GiSeong Lee <cj8928@gmail.com>
2024-09-30 16:59:07 +0900

    update: 챔피언 페이지들 api fetch변경으로 인한 수정

--- src/app/champions/[name]/page.tsx
+++ src/app/champions/[name]/page.tsx
@@ -1,5 +1,20 @@
-const ChampionDetailPage = () => {
-  return <div>detail</div>;
+import ChampionDetail from "@/components/detail/ChampionDetail";
+import { ChampionDetailResponseType } from "@/types/championType";
+import { getChampionDetail } from "@/utils/serverApi";
+
+export const dynamic = "force-dynamic";
+
+interface ParamsType {
+  name: string;
+}
+
+const ChampionDetailPage = async ({ params }: { params: ParamsType }) => {
+  const data: ChampionDetailResponseType = await getChampionDetail(params.name);
+  return (
+    <div className="container">
+      <ChampionDetail data={data} />
+    </div>
+  );
 };
 
 export default ChampionDetailPage;
