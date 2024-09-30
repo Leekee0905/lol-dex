b3ff79918d53fa5b460a596cd99ee0651c274ef2
GiSeong Lee <cj8928@gmail.com>
2024-09-30 16:58:08 +0900

    update: fetch 사용으로 인한 api재생성

---
+++ src/utils/serverApi.ts
@@ -0,0 +1,56 @@
+import {
+  ChampionDetailResponseType,
+  ChampionListResponseType,
+} from "@/types/championType";
+
+export const getChampionsList = async () => {
+  const versionResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_RIOT_API_URL}/api/versions.json`,
+    {
+      method: "GET",
+      headers: {
+        "X-Riot-Token": process.env.NEXT_PUBLIC_RIOT_API_KEY || "",
+      },
+    }
+  );
+  const version: string[] = await versionResponse.json();
+
+  const championListResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_RIOT_API_URL}/cdn/${version[0]}/data/ko_KR/champion.json`,
+    {
+      method: "GET",
+      headers: {
+        "X-Riot-Token": process.env.NEXT_PUBLIC_RIOT_API_KEY || "",
+      },
+    }
+  );
+  const championList: ChampionListResponseType =
+    await championListResponse.json();
+  return championList.data;
+};
+
+export const getChampionDetail = async (name: string) => {
+  const versionResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_RIOT_API_URL}/api/versions.json`,
+    {
+      method: "GET",
+      headers: {
+        "X-Riot-Token": process.env.NEXT_PUBLIC_RIOT_API_KEY || "",
+      },
+    }
+  );
+  const version: string[] = await versionResponse.json();
+  const championDetailResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_RIOT_API_URL}/cdn/${version[0]}/data/ko_KR/champion/${name}.json`,
+    {
+      method: "GET",
+      headers: {
+        "X-Riot-Token": process.env.NEXT_PUBLIC_RIOT_API_KEY || "",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const championDetail: ChampionDetailResponseType =
+    await championDetailResponse.json();
+  return championDetail;
+};
