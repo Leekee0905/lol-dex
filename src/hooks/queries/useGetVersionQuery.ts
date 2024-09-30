767dce0b6ef531665e22d227f46adb256fbf3928
GiSeong Lee <cj8928@gmail.com>
2024-09-30 16:57:22 +0900

    delete: axios 미사용 => fetch전환, 렌더링 기법 사용에 의한 query삭제

--- src/hooks/queries/useGetVersionQuery.ts
+++
@@ -1,9 +0,0 @@
-import { getLatestVersion } from "@/utils/ddragonApi";
-import { useQuery } from "@tanstack/react-query";
-
-export const useGetVersionQuery = () => {
-  return useQuery({
-    queryKey: ["version"],
-    queryFn: getLatestVersion,
-  });
-};
