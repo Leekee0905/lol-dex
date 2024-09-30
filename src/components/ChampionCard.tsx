6bb3a39d9ade2f61b40db1126c1b16bca950567e
GiSeong Lee <cj8928@gmail.com>
2024-09-30 17:39:11 +0900

    update: 챔피언 카드 서버 렌더링으로 변경

--- src/components/ChampionCard.tsx
+++ src/components/ChampionCard.tsx
@@ -1,27 +1,33 @@
-"use client";
-
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import Image from "next/image";
-import { useQueryClient } from "@tanstack/react-query";
 import { ChampionType } from "@/types/championType";
+import Link from "next/link";
 
 const ChampionCard = ({ data }: { data: ChampionType }) => {
-  const queryClient = useQueryClient();
-  const version: string = queryClient.getQueryData(["version"])!;
   return (
-    <Card className="flex flex-col items-center justify-center">
-      <CardHeader>
-        <Image
-          src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${data.image.full}`}
-          width={50}
-          height={50}
-          alt={data.id}
-        />
-      </CardHeader>
-      <CardTitle>{data.name}</CardTitle>
-      <CardDescription>{data.title}</CardDescription>
-      <CardContent></CardContent>
-    </Card>
+    <Link href={`/champions/${data.id}`}>
+      <Card className="flex flex-col items-center justify-center">
+        <CardHeader>
+          <Image
+            src={`https://ddragon.leagueoflegends.com/cdn/${data.version}/img/champion/${data.image.full}`}
+            width={100}
+            height={100}
+            alt={data.id}
+          />
+        </CardHeader>
+        <CardTitle>{data.name}</CardTitle>
+
+        <CardContent>
+          <CardDescription>{data.title}</CardDescription>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 
