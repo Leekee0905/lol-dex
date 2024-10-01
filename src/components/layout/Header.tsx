b2338b2a6415d59b43e37818a549ac1f695df3a5
GiSeong Lee <cj8928@gmail.com>
2024-10-01 19:31:40 +0900

    test: 헤더 다크모드 테스트 에러 주석처리

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,34 @@
+// "use client";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { IoMdSunny } from "react-icons/io";
+import { MdModeNight } from "react-icons/md";
 
 const Header = () => {
+  // const [mounted, setMounted] = useState<boolean>(false);
+  // const { theme, setTheme } = useTheme();
+  // useEffect(() => {
+  //   setMounted(true);
+  // }, []);
+
+  // if (!mounted) {
+  //   return null;
+  // }
   return (
     <header className="bg-gray-800 text-white py-4 px-10 fixed top-0 w-full z-10">
-      <nav className="flex justify-between items-center ">
-        <Link href={"/"}>
+      <nav className="flex justify-between items-center">
+        <Link href={"/"} className="flex items-center justify-center">
           <Image
             src={"/assets/logo.png"}
             width={50}
             height={50}
             alt="홈 로고"
           />
+          <p>롤 백과사전</p>
         </Link>
-        <ul className="flex w-full justify-end items-center gap-10">
+        <ul className="flex justify-end items-center gap-10">
           <li>
             <Link href={"/rotation"}>로테이션</Link>
           </li>
@@ -23,6 +38,19 @@ const Header = () => {
           <li>
             <Link href={"/items"}>아이템 목록</Link>
           </li>
+          {/* <li>
+            {theme === "dark" ? (
+              <IoMdSunny
+                className="cursor-pointer"
+                onClick={() => setTheme("light")}
+              />
+            ) : (
+              <MdModeNight
+                className="cursor-pointer"
+                onClick={() => setTheme("dark")}
+              />
+            )}
+          </li> */}
         </ul>
       </nav>
     </header>
