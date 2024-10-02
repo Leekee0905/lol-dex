9fb06f4dafcbe4e0cc34e0d21eee87ef18c2ab32
GiSeong Lee <cj8928@gmail.com>
2024-10-02 20:06:50 +0900

    style: grid 반응형 구현

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -10,7 +10,7 @@ const ChampionsPage = async () => {
   return (
     <div className="container mt-10">
       <h1 className="text-[#ff5555] font-bold">챔피언 목록</h1>
-      <div className="container grid grid-cols-5 gap-5 py-5">
+      <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 py-5">
         {data.map((champion: ChampionType) => {
           return <ChampionCard key={champion.id} data={champion} />;
         })}
