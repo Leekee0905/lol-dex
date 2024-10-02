cc0bca1a0c605cd11d5683cbbe965af25965a59a
GiSeong Lee <cj8928@gmail.com>
2024-10-02 20:06:14 +0900

    update: 반응형 헤더 햄버거 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,59 +1,50 @@
-// "use client";
-import { useTheme } from "next-themes";
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { IoMdSunny } from "react-icons/io";
-import { MdModeNight } from "react-icons/md";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import HiddenHeaderList from "./HiddenHeaderList";
 
 const Header = () => {
-  // const [mounted, setMounted] = useState<boolean>(false);
-  // const { theme, setTheme } = useTheme();
-  // useEffect(() => {
-  //   setMounted(true);
-  // }, []);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  // if (!mounted) {
-  //   return null;
-  // }
   return (
-    <header className="bg-gray-800 text-white py-4 px-10 fixed top-0 w-full z-10">
-      <nav className="flex justify-between items-center">
-        <Link href={"/"} className="flex items-center justify-center">
-          <Image
-            src={"/assets/logo.png"}
-            width={50}
-            height={50}
-            alt="홈 로고"
-          />
-          <p>롤 백과사전</p>
-        </Link>
-        <ul className="flex justify-end items-center gap-10">
-          <li>
-            <Link href={"/rotation"}>로테이션</Link>
-          </li>
-          <li>
-            <Link href={"/champions"}>챔피언 목록</Link>
-          </li>
-          <li>
-            <Link href={"/items"}>아이템 목록</Link>
-          </li>
-          {/* <li>
-            {theme === "dark" ? (
-              <IoMdSunny
-                className="cursor-pointer"
-                onClick={() => setTheme("light")}
-              />
-            ) : (
-              <MdModeNight
-                className="cursor-pointer"
-                onClick={() => setTheme("dark")}
-              />
-            )}
-          </li> */}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="bg-gray-800 text-white py-4 px-10 sticky top-0 w-full z-10 flex flex-col">
+        <nav className="flex justify-between items-center">
+          <Link href={"/"} className="flex items-center justify-center">
+            <Image
+              src={"/assets/logo.png"}
+              width={50}
+              height={50}
+              alt="홈 로고"
+            />
+            <p>롤 백과사전</p>
+          </Link>
+
+          <button
+            className="block md:hidden"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <RxHamburgerMenu />
+          </button>
+
+          <ul className="hidden md:flex justify-end items-center gap-10">
+            <li>
+              <Link href={"/rotation"}>로테이션</Link>
+            </li>
+            <li>
+              <Link href={"/champions"}>챔피언 목록</Link>
+            </li>
+            <li>
+              <Link href={"/items"}>아이템 목록</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      {isOpen && <HiddenHeaderList onClick={() => setIsOpen(false)} />}
+    </>
   );
 };
 
